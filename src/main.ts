800a3bf5f103c47ed997d30ff447918fba451c43
Hadon <121899758+Hadon20@users.noreply.github.com>
2025-08-12 17:04:55 +0100

    Added the operation logic

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+type Operator = '+' | '-' | '*' | '/'
+
+
+let firstNumber: number
+let secondNumber: number
+const OPERATORS = {
+    addition: '+' as Operator,
+    subtraction: '-' as Operator,
+    multiplication: '*' as Operator,
+    division: '/' as Operator
+}
+
+function addition(firstNumber: number, secondNumber: number) {
+    return firstNumber + secondNumber
+}
+
+function subtraction(firstNumber: number, secondNumber: number) {
+    return firstNumber - secondNumber
+}
+
+function multiplication(firstNumber: number, secondNumber: number) {
+    return firstNumber * secondNumber
+}
+
+function division(firstNumber: number, secondNumber: number) {
+    if (secondNumber === 0) {
+        throw new Error('Division by zero is not allowed')
+    }
+    return firstNumber / secondNumber
+}
+
+function operate(firstNumber: number, secondNumber: number, operator: Operator) {
+    switch (operator) {
+        case OPERATORS.addition:
+            return addition(firstNumber, secondNumber)
+        case OPERATORS.subtraction:
+            return subtraction(firstNumber, secondNumber)
+        case OPERATORS.multiplication:
+            return multiplication(firstNumber, secondNumber)
+        case OPERATORS.division:
+            return division(firstNumber, secondNumber)
+        default:
+            throw new Error(`Unknown Error: ${operator}`)
+    }
+}
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/Etch-a-Sketch/',
+    base: '/calculator/',
     build: {
         outDir: 'docs'
     }
